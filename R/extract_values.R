6c8a6cab1472a405d0a0f69f7c72198fa7c2fcaf
cjhzhang <zhangjhcathy@gmail.com>
2026-03-25 18:16:26 -0700

    abstracting function to extract values: wrote documentation and specifications, tests, helpers for tests

---
+++ R/extract_values.R
@@ -0,0 +1,19 @@
+#' Extract values from a certain named field 
+#' 
+#' Returns a vector with the values under a certain field name
+#' 
+#' @param object can be either a data frame, 
+#' a list of sub lists, NULL, or a zero-length object
+#' @param field_name the field name to extract values from, 
+#' default is 'category'
+#' 
+#' @return A vector of the extracted values. Returns character(0) if
+#' the input is null, empty, or does not contain the specified field name
+#' 
+#' @export 
+#' 
+#' @examples 
+#' 
+extract_values <- function(object, field_name = 'category' ) {
+    #Returns a vector with the values under a certain field name
+}
---
+++ tests/testthat/helper-extract_values.R
@@ -0,0 +1,67 @@
+# reproducible code to generate simple test data 
+# for the extract values function
+
+#expected case 1, dataframe with one column of field name, with one value
+extract_values_exp_1_df_1col <- data.frame(
+    category = "Co-op"
+)
+
+ev_exp_output_1 <- c("Co-op")
+
+#expected case 2, dataframe with two columns, one of field name, duplicated categories
+extract_values_exp_2_df_2col <- data.frame(
+    category = c("Co-op", "Singleplayer", "Singleplayer"),
+    game_name = c("Game1", "Game2", "Game3")
+)
+
+ev_exp_output_2 <- c("Co-op", "Singleplayer", "Singleplayer")
+
+#expected case 3, nested object, contains 'category', duplicate category
+extract_values_exp_3_nested_list_some_missing <- list(
+    list(game_name = "Game1", category = "Singleplayer"),
+    list(game_name = "Game2", category = "Co-op"),
+    list(game_name = "Game3", category = "Singleplayer"),
+    list(game_name = "Game4", category = "Multiplayer")
+
+)
+
+ev_exp_output_3 <- c("Singleplayer", "Co-op", "Singleplayer", "Multiplayer")
+
+#expected case 4, nested lists, some have category and some dont
+extract_values_exp_4_nested_list <- list(
+    list(game_name = "Game1", category = "Singleplayer"),
+    list(game_name = "Game2", genre = "Competitive"),
+    list(game_name = "Game3", category = "Co-op"),
+    list()
+
+)
+
+ev_exp_output_4 <- c("Singleplayer", "Co-op")
+
+#expected empty 
+
+#null
+extract_values_null_input <- NULL
+
+#edge case 1: list of list 0 long 
+extract_values_edge_0_list <- list()
+
+#edge case 2: dataframe without category column 
+extract_values_edge_df_no_category <- data.frame(
+     game_name = c("Game1", "Game2", "Game3"),
+     game_genre = c("Singleplayer", "Co-op", "Multiplayer")
+
+)
+
+#edge case 3: list of lists without category
+extract_values_edge_list_no_category <- list(
+    list(game_name = "Game1", game_genre = "Singleplayer"),
+    list(game_name = "Game2", game_genre = "Multiplayer"),
+    list(game_name = "Game3", game_genre = "Co-op")
+)
+
+#edge case 4: vector rather than df or list of lists
+extract_values_edge_vector_input <- c("Singleplayer", "Co-op", "Multiplayer")
+
+ev_exp_output_empty <- character(0)
+
